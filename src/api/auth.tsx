27b94a198fb60b1e763187879b8d09a2caef26b6
anas <muhammadanasrashid18@gmail.com>
2025-01-22 18:54:12 +0500

    ...

--- src/api/auth.tsx
+++ src/api/auth.tsx
@@ -1,14 +1,16 @@
 import axios from "axios";
-import { development } from "./config";
+import { development ,production} from "./config";
 
 interface PropsLogin {
   email: string;
   password: string;
 }
 
+const server = null;
+
 export const logInApi = async ({ email, password }: PropsLogin) => {
   try {
-    const res = await axios.post(`${development}/auth/signin/customer`, {
+    const res = await axios.post(`${  server ? production : development}/auth/signin/customer`, {
       email: email,
       password: password,
     });
