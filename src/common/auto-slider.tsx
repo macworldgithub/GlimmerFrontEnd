e239e99c8e1fe3c55878d88d4581f403472e6e53
hammadlodhi <hammadak05@gmail.com>
2025-03-26 01:04:25 +0500

    selfcard

--- src/common/auto-slider.tsx
+++ src/common/auto-slider.tsx
@@ -5,9 +5,11 @@ import HeroImg2 from "@/assets/images/home-hero-img-2.webp";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectFade } from "swiper/modules";
 import Link from "next/link";
+import "swiper/css";
+import "swiper/css/effect-fade"; // Import required styles
 
 type Props = {
-	srcs?: string[];
+  srcs?: string[];
 	type?:
 		| "slide"
 		| "fade"
@@ -16,38 +18,44 @@ type Props = {
 		| "flip"
 		| "creative"
 		| "cards";
-	delay?: number | null;
+  delay?: number | null;
 };
 
 const AutoSlider = ({ srcs = [], type = "slide", delay = 3000 }: Props) => {
-	const _srcs = srcs.length > 0 ? srcs : [HeroImg1.src, HeroImg2.src];
-	const modules = [];
-	if (delay) modules.push(Autoplay);
-	if (type === "fade") modules.push(EffectFade);
-	return (
-		<Swiper
-			modules={modules}
-			spaceBetween={50}
-			slidesPerView={1}
+  const _srcs = srcs.length > 0 ? srcs : [HeroImg1.src, HeroImg2.src];
+  const modules = [];
+  if (delay) modules.push(Autoplay);
+  if (type === "fade") modules.push(EffectFade);
+  return (
+    <div className="relative w-full max-w-screen-xl mx-auto overflow-hidden rounded-lg shadow-lg">
+      <Swiper
+        modules={modules}
+        spaceBetween={50}
+        slidesPerView={1}
 			// onSlideChange={() => console.log("slide change")}
 			// onSwiper={(swiper) => console.log(swiper)}
-			effect={type}
-			onError={(e) => console.log("[hero swiper] error: ", e)}
-			autoplay={{ delay: delay as number }}
-			cssMode={true}
-			className=" mb-6 w-full md:mb-8 rounded-lg"
-		>
-			{_srcs.map((s, index) => (
-				<SwiperSlide key={s}>
+        effect={type}
+        autoplay={{ delay: delay as number }}
+        loop={true}
+        cssMode={true}
+        className="w-full"
+      >
+        {_srcs.map((s, index) => (
+          <SwiperSlide key={s}>
 					<div className="w-full">
-						<Link href={index === 0 ? "/salons" : "/selfcare-products"}>
-							<img src={s} className="w-full" alt="Swiper Carousel component" />
-						</Link>
-					</div>
-				</SwiperSlide>
-			))}
-		</Swiper>
-	);
+              <Link href={index === 0 ? "/salons" : "/selfcare-products"}>
+                <img
+                  src={s}
+                  className="w-full h-full object-cover rounded-lg transition-transform duration-500 hover:scale-105 hover:brightness-110"
+                  alt="Swiper Carousel component"
+                />
+              </Link>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default AutoSlider;
