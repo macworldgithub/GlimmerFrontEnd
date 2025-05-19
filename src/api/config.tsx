b2edfe8e5b97cd9844a6b7cb277279756542ae17
shweeta <shweeta@macworldtechnology.com>
2025-05-19 13:22:46 +0500

    url changed

--- src/api/config.tsx
+++ src/api/config.tsx
@@ -1,9 +1,9 @@
 export const development = "http://localhost:3000";
-export const production = "https://api.glimmer.com.pk";
+export const production = "https://www.api.glimmer.com.pk";
 
 const curr_env: "Prod" | "DEV" = "Prod";
 
-// export const BACKEND_URL =
-//   //@ts-ignore
-//   curr_env === "DEV" ? development : production;
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL =
+  //@ts-ignore
+  curr_env === "DEV" ? development : production;
+// export const BACKEND_URL = "http://localhost:3000";
