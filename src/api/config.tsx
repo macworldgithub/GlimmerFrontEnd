68ebb65f0cb93bb8e6e438c7a6b088c3ec9e71a6
shweeta <shweeta@macworldtechnology.com>
2025-05-29 12:38:43 +0500

    chnged

--- src/api/config.tsx
+++ src/api/config.tsx
@@ -1,8 +1,8 @@
-export const developmentServer = "http://localhost:3000";
+// export const developmentServer = "http://localhost:3000";
 export const productionServer = "https://www.api.glimmer.com.pk";
 
 // Assign a value to `curr_env`, not a type
-const curr_env: "Prod" | "DEV" = "DEV";
+const curr_env: "Prod" | "DEV" = "Prod";
 
 export const BACKEND_URL =
   //@ts-ignore
