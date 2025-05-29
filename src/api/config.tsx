cdf3943f5aa441ccf4d4c1d998acc804a25c1f8b
shweeta <shweeta@macworldtechnology.com>
2025-05-29 16:27:51 +0500

    doneee

--- src/api/config.tsx
+++ src/api/config.tsx
@@ -6,5 +6,5 @@ const curr_env: "Prod" | "DEV" = "DEV";
 
 export const BACKEND_URL =
   //@ts-ignore
-curr_env === "DEV" ? developmentServer : productionServer;
+curr_env === "DEV" ? development : production;
 // export const BACKEND_URL = "http://localhost:3000";
