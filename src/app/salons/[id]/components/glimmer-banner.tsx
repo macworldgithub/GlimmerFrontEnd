611d8fd10452c7fcce89a6ddfd11db05cb1394ed
Shweeta587 <shweeta@macworldtechnologies.com>
2025-06-11 13:36:11 +0500

    Sizes Adjusted

--- src/app/salons/[id]/components/glimmer-banner.tsx
+++ src/app/salons/[id]/components/glimmer-banner.tsx
@@ -11,8 +11,8 @@ const GlimmerBanner = ({ srcs = [] }: Props) => {
   const _srcs = srcs.length > 0 ? srcs : [glimmerBannerImg.src];
 
   return (
-    <div className="w-[125%] px-4 md:px-10 md:mb-14 flex justify-center pb-10">
-      <div className="max-w-[1300px] w-[100%]">
+    <div className="w-full flex justify-center items-center px-4 md:px-10 md:mb-14 pb-10">
+      <div className="max-w-[1000px] w-[100%]">
         <AutoSlider srcs={_srcs} />
       </div>
     </div>
