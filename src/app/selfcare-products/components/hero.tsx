ee10d079043d915190e5ca53b7700a1f67fed2a4
Shweeta587 <shweeta@macworldtechnologies.com>
2025-06-27 13:46:06 +0500

    Products page routed

--- src/app/selfcare-products/components/hero.tsx
+++ src/app/selfcare-products/components/hero.tsx
@@ -1,24 +1,35 @@
 "use client";
 import * as React from "react";
+import { useRouter } from "next/navigation";
 import HeroImg1 from "@/assets/selfcare-slider/selfcare-slider-1.png";
 import HeroImg2 from "@/assets/selfcare-slider/selfcare-slider-2.png";
 import HeroImg3 from "@/assets/selfcare-slider/selfcare-slider-3.png";
 import AutoSlider from "@/common/auto-slider";
-import { Breadcrumb } from "antd";
 
 type Props = {
   srcs?: string[];
 };
 
 const Hero = ({ srcs = [] }: Props) => {
+  const router = useRouter();
+
   const _srcs =
     srcs.length > 0 ? srcs : [HeroImg1.src, HeroImg2.src, HeroImg3.src];
 
+  // Custom function to be passed only for first banner
+  const handleFirstBannerClick = () => {
+    router.push("/products");
+  };
+
   return (
     <div className="w-screen overflow-x-hidden">
-   
-      <AutoSlider srcs={_srcs} />
-    
+      {/* Only pass onBannerClick when first image is shown */}
+      <AutoSlider
+        srcs={_srcs}
+        onBannerClick={
+          _srcs[0] === HeroImg1.src ? handleFirstBannerClick : undefined
+        }
+      />
     </div>
   );
 };
