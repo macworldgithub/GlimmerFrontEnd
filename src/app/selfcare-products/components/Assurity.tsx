c198d55b0b1af8672372e75b4bef43f66893d179
hammadlodhi <hammadak05@gmail.com>
2025-07-16 15:41:55 +0500

    remove assurity logos from mobile

--- src/app/selfcare-products/components/Assurity.tsx
+++ src/app/selfcare-products/components/Assurity.tsx
@@ -21,7 +21,8 @@ const Assurity = () => {
     );
   }
   return (
-    <div className="w-[99vw]  px-[8rem] py-6 flex justify-center items-center max-md:flex-col h-max">
+  <div className="hidden md:flex w-[99vw] px-[8rem] py-6 justify-center items-center flex-col md:flex-row h-max">
+
       {assurances.map((assurance, index) => (
         <div key={index} className="flex gap-2 items-center max-md:flex-col">
           <Image
