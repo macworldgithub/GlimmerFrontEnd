ae7c56af51302ace6c67cd4d5d2e2c2779117e72
HUZAIFA <karimjawwad09@gmail.com>
2025-07-24 16:30:23 +0500

    ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,29 +9,22 @@ import CookieBanner from "@/common/cookie-banner";
 import ClientLayout from "@/common/client-layout";
 import FloatingWhatsApp from "@/common/FloatingWhatsApp";
 import ChatbotWidget from "@/common/ChatbotWidget";
-import Script from "next/script";  // <-- import Script here
+// import FloatingChatbot from '@/common/FloatingChatbot';
 
 const prompt = Prompt({
   subsets: ["latin"],
-  weight: [
-    "100",
-    "200",
-    "300",
-    "400",
-    "500",
-    "600",
-    "700",
-    "800",
-    "900",
-  ],
+  weight: ["100", "200", "300", "400", "500", "600", "700","800","900"],
   display: "swap",
   variable: "--font-prompt",
 });
 
+
 export const metadata: Metadata = {
-  title: "www.glimmer.com.pk",
+  title:
+    "www.glimmer.com.pk",
   description:
     "Make an online salon appointment at The Best Prices Online Cosmetics Store in Pakistan by visiting glimmer.com.pk.",
+    
 };
 
 export default function RootLayout({
@@ -42,26 +35,13 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="light">
       <body className={`${prompt.variable} container bg-base-100 antialiased`}>
-        {/* Google Analytics Scripts */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-HG69Q30FKD"
-          strategy="afterInteractive"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-HG69Q30FKD');
-          `}
-        </Script>
-
         <CartStoreProvider>
           <Navbar />
           <ClientLayout>{children}</ClientLayout>
           <CookieBanner />
           <ChatbotWidget />
         </CartStoreProvider>
+      
       </body>
     </html>
   );
