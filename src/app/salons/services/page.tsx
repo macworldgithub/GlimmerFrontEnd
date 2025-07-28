01d8261a77e7376eed04920c102b4185b8f1bdf2
shweeta <shweeta@macworldtechnology.com>
2025-07-28 13:31:47 +0500

    ...

--- src/app/salons/services/page.tsx
+++ src/app/salons/services/page.tsx
@@ -388,7 +388,6 @@ const ServiceList = () => {
 
           {/* Service Grid */}
           <div className="w-full h-max grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-x-4 gap-y-6 p-4">
-
             {data.length ? (
               data.map((item) => (
                 <motion.div
