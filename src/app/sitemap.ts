5cec111730a4a036a414bbf7b40fd4f56cce42e6
hammadlodhi <hammadak05@gmail.com>
2025-09-24 13:39:19 +0500

    frontend url added

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,41 +1,41 @@
 import { MetadataRoute } from "next";
 import { getAllProductItem, getAllProducts } from "@/api/product";
 import { fetchAllSalons, fetchSalonServices } from "@/api/salon";
-import { BACKEND_URL } from "@/api/config";
+import { FRONTEND_URL } from "@/api/config";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const urls: MetadataRoute.Sitemap = [];
 
   urls.push({
-    url: BACKEND_URL,
+    url: FRONTEND_URL,
     lastModified: new Date(),
     changeFrequency: "daily",
     priority: 1,
   });
 
   urls.push({
-    url: `${BACKEND_URL}/selfcare-products`,
+    url: `${FRONTEND_URL}/selfcare-products`,
     lastModified: new Date(),
     changeFrequency: "weekly",
     priority: 0.9,
   });
 
   urls.push({
-    url: `${BACKEND_URL}/Blogs`,
+    url: `${FRONTEND_URL}/Blogs`,
     lastModified: new Date(),
     changeFrequency: "weekly",
     priority: 0.7,
   });
 
   urls.push({
-    url: `${BACKEND_URL}/product_highlights`,
+    url: `${FRONTEND_URL}/product_highlights`,
     lastModified: new Date(),
     changeFrequency: "weekly",
     priority: 0.7,
   });
 
   urls.push({
-    url: `${BACKEND_URL}/products`,
+    url: `${FRONTEND_URL}/products`,
     lastModified: new Date(),
     changeFrequency: "weekly",
     priority: 0.8,
@@ -50,7 +50,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
       for (const salon of salons) {
         if (salon.city_slug && salon.salon_slug) {
-          const baseSalonUrl = `${BACKEND_URL}/${salon.city_slug}/${salon.salon_slug}`;
+          const baseSalonUrl = `${FRONTEND_URL}/${salon.city_slug}/${salon.salon_slug}`;
 
           urls.push({
             url: baseSalonUrl,
@@ -110,7 +110,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   for (const cat of categories) {
     urls.push({
-      url: `${BACKEND_URL}/${cat.product_category.slug}`,
+      url: `${FRONTEND_URL}/${cat.product_category.slug}`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
@@ -118,7 +118,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
     for (const sub of cat.sub_categories) {
       urls.push({
-        url: `${BACKEND_URL}/${cat.product_category.slug}/${sub.slug}`,
+        url: `${FRONTEND_URL}/${cat.product_category.slug}/${sub.slug}`,
         lastModified: new Date(),
         changeFrequency: "weekly",
         priority: 0.7,
@@ -126,7 +126,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
       for (const item of sub.items) {
         urls.push({
-          url: `${BACKEND_URL}/${cat.product_category.slug}/${sub.slug}/${item.slug}`,
+          url: `${FRONTEND_URL}/${cat.product_category.slug}/${sub.slug}/${item.slug}`,
           lastModified: new Date(),
           changeFrequency: "weekly",
           priority: 0.6,
@@ -149,7 +149,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
           if (res?.products?.length) {
             for (const product of res.products) {
               urls.push({
-                url: `${BACKEND_URL}/${cat.product_category.slug}/${sub.slug}/${item.slug}/${product._id}`,
+                url: `${FRONTEND_URL}/${cat.product_category.slug}/${sub.slug}/${item.slug}/${product._id}`,
                 lastModified: new Date(
                   product.updated_at || product.created_at || new Date()
                 ),
